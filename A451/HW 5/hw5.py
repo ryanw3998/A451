0cffaaab83510654c6765edbdb4bb76157bace6f
ryanwebs <ryanwebs@mail-relay.iu.edu>
2021-11-11 13:27:40 -0500

    Homework 5

---
+++ A451/HW 5/hw5.py
@@ -0,0 +1,48 @@
+'''
+Ryan Webster
+Script for written homework #5
+Problem 2
+'''
+
+
+'''
+Note: I converted the xlsx file to a csv
+for better compatability with pandas
+'''
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# read in data
+data = pd.read_csv('HW5_Data.csv')
+
+# Extract column information
+date = data['HJD-2450000']
+mag = data['mag']
+error = data['error']
+
+# Plot
+plt.scatter(date,mag,s=2)
+plt.xlabel('HJD-2450000')
+plt.ylabel('Mag')
+plt.show()
+
+# These values were found by eye
+# on the plot
+n_peaks = 11
+date1 = 2106
+date2 = 2225
+
+period = np.round((date2-date1)/n_peaks,2)
+print(period)
+
+phase = ((date-date1)/period) % 1
+print(((date-date1)/period))
+print(phase)
+
+
+plt.scatter(phase,mag,s=2)
+plt.xlabel('Phase')
+plt.ylabel('Mag')
+plt.show()
